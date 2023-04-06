699e1dfac0ea4dc8608d3ef6ec044350070e6609
harshadvali11 <harshadvali1431@gmail.com>
2023-04-06 17:06:54 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from app.models import *
 # Create your views here.
+from django.db.models.functions import Length
 
 def display_topics(request):
     LOT=Topic.objects.all()
@@ -9,10 +10,28 @@ def display_topics(request):
 
 def display_webpages(request):
     LOW=Webpage.objects.all()
+    LOW=Webpage.objects.filter(topic_name='Cricket')
+    #LOW=Webpage.objects.get(topic_name='Chess')
+    LOW=Webpage.objects.exclude(topic_name='Cricket')
+    LOW=Webpage.objects.all()[1:2:]
+    LOW=Webpage.objects.all().order_by('name')
+    LOW=Webpage.objects.all().order_by('-name')
+    
+    LOW=Webpage.objects.all().order_by(Length('name'))
+    #LOW=Webpage.objects.all().
+    LOW=Webpage.objects.all().order_by(Length('name').desc())
+    
     d={'webpages':LOW}
     return render(request,'display_webpages.html',d)
+
+
 def display_access(request):
     LOA=AccessRecord.objects.all()
+    LOA=AccessRecord.objects.filter(date__gt='2022-10-10')
+    LOA=AccessRecord.objects.filter(date__gte='2022-10-10')
+    LOA=AccessRecord.objects.filter(date__lt='2022-10-10')
+    LOA=AccessRecord.objects.filter(date__lte='2022-10-10')
+    
     d={'access':LOA}
     return render(request,'display_access.html',d)
 
